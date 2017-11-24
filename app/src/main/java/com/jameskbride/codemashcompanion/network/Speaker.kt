ef9ce25d7464fac5d0d365b941840fd2c20fb157
Jim Kirkbride <james.kirkbride@gmail.com>
2017-11-24 11:23:48 -0500

    Cleaning up the Speaker data class.

--- app/src/main/java/com/jameskbride/codemashcompanion/network/Speaker.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/network/Speaker.kt
@@ -1,14 +1,14 @@
 package com.jameskbride.codemashcompanion.network
 
 data class Speaker(
-        var linkedInProfile: String? = null,
-        var id: String? = null,
-        var lastName: String? = null,
-        var sessionIds: Array<String> = arrayOf(String()),
-        var twitterLink: String? = null,
-        var gitHubLink: String? = null,
-        var firstName: String? = null,
-        var gravatarUrl: String? = null,
-        var biography: String? = null,
-        var blogUrl: String? = null
+        val linkedInProfile: String = "",
+        val id: String = "",
+        val lastName: String = "",
+        val sessionIds: Array<String> = arrayOf(String()),
+        val twitterLink: String = "",
+        val gitHubLink: String = "",
+        val firstName: String = "",
+        val gravatarUrl: String = "",
+        val biography: String = "",
+        val blogUrl: String = ""
 )
