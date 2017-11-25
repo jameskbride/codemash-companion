61f3c7853b9242f4febfb8abb2a2040e33654d12
Jim Kirkbride <james.kirkbride@gmail.com>
2017-11-24 21:53:02 -0500

    Marking fields as optional.

--- app/src/main/java/com/jameskbride/codemashcompanion/network/Speaker.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/network/Speaker.kt
@@ -9,14 +9,14 @@ import android.arch.persistence.room.PrimaryKey
 data class Speaker(
         @ColumnInfo(name = "id") @PrimaryKey var Id: String = "",
 
-        @ColumnInfo(name = "linkedin_profile") var LinkedInProfile: String = "",
-        @ColumnInfo(name = "last_name") var LastName: String = "",
-        @ColumnInfo(name = "twitter_link") var TwitterLink: String = "",
-        @ColumnInfo(name = "github_link") var GitHubLink: String = "",
-        @ColumnInfo(name = "first_name") var FirstName: String = "",
-        @ColumnInfo(name = "gravatar_url") var GravatarUrl: String = "",
-        @ColumnInfo(name = "biography") var Biography: String = "",
-        @ColumnInfo(name = "blog_url") var BlogUrl: String = "",
+        @ColumnInfo(name = "linkedin_profile") var LinkedInProfile: String? = "",
+        @ColumnInfo(name = "last_name") var LastName: String? = "",
+        @ColumnInfo(name = "twitter_link") var TwitterLink: String? = "",
+        @ColumnInfo(name = "github_link") var GitHubLink: String? = "",
+        @ColumnInfo(name = "first_name") var FirstName: String? = "",
+        @ColumnInfo(name = "gravatar_url") var GravatarUrl: String? = "",
+        @ColumnInfo(name = "biography") var Biography: String? = "",
+        @ColumnInfo(name = "blog_url") var BlogUrl: String? = "",
 
-        @Ignore var SessionIds: Array<String> = arrayOf(String())
+        @Ignore var SessionIds: Array<String>? = arrayOf(String())
 )
