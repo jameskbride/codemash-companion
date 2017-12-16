3f6ed776598948d47b46f2694e839d54ce785a0f
Jim Kirkbride <james.kirkbride@gmail.com>
2017-12-16 14:33:30 -0500

    Spelling.

--- app/src/main/java/com/jameskbride/codemashcompanion/sessions/SessionsFragmentPresenter.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/sessions/SessionsFragmentPresenter.kt
@@ -9,13 +9,13 @@ import javax.inject.Inject
 class SessionsFragmentPresenter @Inject constructor(val eventBus: EventBus,
                                                     val conferenceRepository: ConferenceRepository,
                                                     val processScheduler: Scheduler,
-                                                    val androidScheuler: Scheduler) {
+                                                    val androidScheduler: Scheduler) {
     lateinit var view: SessionsFragmentView
 
     fun requestSessions() {
         conferenceRepository.getSessions()
                 .subscribeOn(processScheduler)
-                .observeOn(androidScheuler)
+                .observeOn(androidScheduler)
                 .subscribe { results -> notifyView(results) }
     }
 
