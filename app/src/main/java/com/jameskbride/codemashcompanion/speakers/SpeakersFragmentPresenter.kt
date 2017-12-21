7327deab0282e01607d6c755080189fbf0b3a8a0
Jim Kirkbride <james.kirkbride@gmail.com>
2017-12-21 15:07:23 -0500

    Speakers presenter refreshes conference data.

--- app/src/main/java/com/jameskbride/codemashcompanion/speakers/SpeakersFragmentPresenter.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/speakers/SpeakersFragmentPresenter.kt
@@ -1,10 +1,13 @@
 package com.jameskbride.codemashcompanion.speakers
 
 import com.jameskbride.codemashcompanion.bus.BusAware
+import com.jameskbride.codemashcompanion.bus.ConferenceDataPersistedEvent
+import com.jameskbride.codemashcompanion.bus.RequestConferenceDataEvent
 import com.jameskbride.codemashcompanion.data.ConferenceRepository
 import com.jameskbride.codemashcompanion.data.model.FullSpeaker
 import io.reactivex.Scheduler
 import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
 import javax.inject.Inject
 
 class SpeakersFragmentPresenter @Inject constructor(override val eventBus: EventBus,
@@ -26,7 +29,12 @@ class SpeakersFragmentPresenter @Inject constructor(override val eventBus: Event
     }
 
     fun refreshConferenceData() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        eventBus.post(RequestConferenceDataEvent())
+    }
+
+    @Subscribe
+    fun onConferenceDataPersistedEvent(conferenceDataPersistedEvent: ConferenceDataPersistedEvent) {
+        requestSpeakerData()
     }
 }
 
--- app/src/test/java/com/jameskbride/codemashcompanion/speakers/SpeakersFragmentPresenterTest.kt
+++ app/src/test/java/com/jameskbride/codemashcompanion/speakers/SpeakersFragmentPresenterTest.kt
@@ -1,12 +1,21 @@
 package com.jameskbride.codemashcompanion.speakers
 
+import com.jameskbride.codemashcompanion.bus.ConferenceDataPersistedEvent
+import com.jameskbride.codemashcompanion.bus.RequestConferenceDataEvent
 import com.jameskbride.codemashcompanion.data.ConferenceRepository
+import com.jameskbride.codemashcompanion.data.model.FullSession
+import com.jameskbride.codemashcompanion.data.model.FullSpeaker
+import com.jameskbride.codemashcompanion.sessions.SessionData
 import com.jameskbride.codemashcompanion.utils.test.buildDefaultSpeakers
+import com.nhaarman.mockito_kotlin.argumentCaptor
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Maybe
 import io.reactivex.schedulers.TestScheduler
 import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -14,25 +23,30 @@ import org.mockito.MockitoAnnotations.initMocks
 
 class SpeakersFragmentPresenterTest {
 
-    @Mock
-    private lateinit var conferenceRepository:ConferenceRepository
-
-    @Mock
-    private lateinit var eventBus:EventBus
-
-    @Mock
-    private lateinit var view:SpeakersFragmentView
+    @Mock private lateinit var conferenceRepository:ConferenceRepository
+    @Mock private lateinit var view:SpeakersFragmentView
 
     private lateinit var subject:SpeakersFragmentPresenter
 
     private lateinit var testScheduler: TestScheduler
+    private lateinit var eventBus:EventBus
+    private var requestConferenceDataEventFired: Boolean = false
 
     @Before
     fun setUp() {
         initMocks(this)
         testScheduler = TestScheduler()
+        eventBus = EventBus.getDefault()
+        eventBus.register(this)
         subject = SpeakersFragmentPresenter(eventBus, conferenceRepository, testScheduler, testScheduler)
+        subject.open()
         subject.view = view
+    }
+
+    @After
+    fun tearDown() {
+        eventBus.unregister(this)
+        subject.close()
     }
 
     @Test
@@ -55,5 +69,32 @@ class SpeakersFragmentPresenterTest {
         subject.navigateToDetails(speakers, 0)
 
         verify(view).navigateToDetails(speakers, 0)
+    }
+
+    @Test
+    fun itCanRefreshConferenceData() {
+        subject.refreshConferenceData()
+
+        Assert.assertTrue(requestConferenceDataEventFired)
+    }
+
+    @Test
+    fun onConferenceDataPersistedItRequestsSessions() {
+        val speakers = buildDefaultSpeakers()
+
+        whenever(conferenceRepository.getSpeakers()).thenReturn(Maybe.just(speakers))
+
+        eventBus.post(ConferenceDataPersistedEvent())
+
+        testScheduler.triggerActions()
+
+        val sessionDataCaptor = argumentCaptor<Array<FullSpeaker>>()
+        verify(view).onSpeakerDataRetrieved(sessionDataCaptor.capture())
+        Assert.assertArrayEquals(speakers, sessionDataCaptor.firstValue)
+    }
+
+    @Subscribe
+    fun onRequestConferenceDataEvent(requestConferenceDataEvent: RequestConferenceDataEvent) {
+        this.requestConferenceDataEventFired = true
     }
 }
