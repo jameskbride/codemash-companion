4bea007a39030e3e72036f691a55a40b5ffa7bcd
Jim Kirkbride <james.kirkbride@gmail.com>
2017-12-21 19:24:50 -0500

    Setting error dialog title and message.

--- app/src/main/java/com/jameskbride/codemashcompanion/error/ErrorDialogImpl.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/error/ErrorDialogImpl.kt
@@ -5,13 +5,17 @@ import android.support.annotation.StringRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.jameskbride.codemashcompanion.R
 import java.io.Serializable
 import javax.inject.Inject
 
 class ErrorDialogImpl @Inject constructor() {
-    fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, errorDialog: ErrorDialog): View? {
+    fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, qtn: ErrorDialog): View? {
         val view = inflater.inflate(R.layout.error_dialog, container, false)
+        val errorDialogParams = qtn.arguments?.getSerializable(PARAMETER_BLOCK) as ErrorDialogParams
+        view.findViewById<TextView>(R.id.error_dialog_title).setText(errorDialogParams.title)
+        view.findViewById<TextView>(R.id.error_dialog_message).setText(errorDialogParams.message)
 
         return view
     }
--- app/src/test/java/com/jameskbride/codemashcompanion/error/ErrorDialogImplTest.kt
+++ app/src/test/java/com/jameskbride/codemashcompanion/error/ErrorDialogImplTest.kt
@@ -1,9 +1,12 @@
 package com.jameskbride.codemashcompanion.error
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.jameskbride.codemashcompanion.R
+import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Assert.assertSame
@@ -18,6 +21,8 @@ class ErrorDialogImplTest {
     @Mock private lateinit var viewGroup:ViewGroup
     @Mock private lateinit var qtn:ErrorDialog
     @Mock private lateinit var view: View
+    @Mock private lateinit var title:TextView
+    @Mock private lateinit var message:TextView
 
     private lateinit var subject:ErrorDialogImpl
 
@@ -28,13 +33,34 @@ class ErrorDialogImplTest {
         subject = ErrorDialogImpl()
 
         whenever(layoutInflater.inflate(R.layout.error_dialog, viewGroup, false)).thenReturn(view)
+        whenever(view.findViewById<TextView>(R.id.error_dialog_title)).thenReturn(title)
+        whenever(view.findViewById<TextView>(R.id.error_dialog_message)).thenReturn(message)
     }
 
     @Test
     fun onCreateViewItInflatesTheView() {
+        setupErrorDialogParams()
         val result = subject.onCreateView(layoutInflater, viewGroup, null, qtn)
 
         assertSame(view, result)
         verify(layoutInflater).inflate(R.layout.error_dialog, viewGroup, false)
+    }
+
+    @Test
+    fun onCreateViewSetsTheTitleAndMessage() {
+        val errorDialogParams = setupErrorDialogParams()
+
+        subject.onCreateView(layoutInflater, viewGroup, null, qtn)
+
+        verify(title).setText(errorDialogParams.title)
+        verify(message).setText(errorDialogParams.message)
+    }
+
+    private fun setupErrorDialogParams(): ErrorDialogParams {
+        val errorDialogParams = ErrorDialogParams(title = R.string.oops, message = R.string.no_data_message)
+        val bundle = mock<Bundle>()
+        whenever(qtn.arguments).thenReturn(bundle)
+        whenever(bundle.getSerializable(PARAMETER_BLOCK)).thenReturn(errorDialogParams)
+        return errorDialogParams
     }
 }
