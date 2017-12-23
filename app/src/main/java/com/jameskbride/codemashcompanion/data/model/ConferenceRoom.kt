0b9aa817c2e65d85e539e7d8e60846abdddd2c92
Jim Kirkbride <james.kirkbride@gmail.com>
2017-12-23 18:49:59 -0500

    Configuring foreign keys and indices.

--- app/src/main/java/com/jameskbride/codemashcompanion/data/model/ConferenceRoom.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/data/model/ConferenceRoom.kt
@@ -1,12 +1,11 @@
 package com.jameskbride.codemashcompanion.data.model
 
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.Index
-import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.*
 import java.io.Serializable
 
-@Entity(indices = arrayOf(Index(value = arrayOf("session_id", "name"), unique = true)))
+@Entity(indices = arrayOf(Index(value = arrayOf("session_id", "name"), unique = true)),
+        foreignKeys = arrayOf(ForeignKey(entity = Session::class, parentColumns = arrayOf("id"), childColumns = arrayOf("session_id"), onDelete = ForeignKey.CASCADE))
+)
 data class ConferenceRoom(
         @ColumnInfo(name = "session_id") val sessionId:String,
         @ColumnInfo(name = "name") val name:String,
--- app/src/main/java/com/jameskbride/codemashcompanion/data/model/SessionSpeaker.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/data/model/SessionSpeaker.kt
@@ -1,12 +1,17 @@
 package com.jameskbride.codemashcompanion.data.model
 
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.Index
-import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.*
 import java.io.Serializable
 
-@Entity(indices = arrayOf(Index(value = arrayOf("session_id", "speaker_id"), unique = true)))
+@Entity(indices = arrayOf(
+        Index(value = arrayOf("session_id"), unique = true),
+        Index(value = arrayOf("speaker_id"), unique = true)
+        ),
+        foreignKeys = arrayOf(
+                ForeignKey(entity = Session::class, parentColumns = arrayOf("id"), childColumns = arrayOf("session_id"), onDelete = ForeignKey.CASCADE),
+                ForeignKey(entity = Speaker::class, parentColumns = arrayOf("id"), childColumns = arrayOf("speaker_id"), onDelete = ForeignKey.CASCADE)
+        )
+)
 data class SessionSpeaker constructor(
         @ColumnInfo(name = "session_id") val sessionId:String,
         @ColumnInfo(name = "speaker_id") val speakerId:String,
