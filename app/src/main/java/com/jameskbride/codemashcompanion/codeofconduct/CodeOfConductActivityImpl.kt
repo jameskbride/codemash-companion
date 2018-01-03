f286324f8747958d84679c441be5c2aa17bec986
Jim Kirkbride <james.kirkbride@gmail.com>
2018-01-02 20:43:38 -0500

    Setting the content view.  Loading the code of conduct url.

--- app/src/main/java/com/jameskbride/codemashcompanion/codeofconduct/CodeOfConductActivityImpl.kt
+++ app/src/main/java/com/jameskbride/codemashcompanion/codeofconduct/CodeOfConductActivityImpl.kt
@@ -1,19 +1,32 @@
 package com.jameskbride.codemashcompanion.codeofconduct
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.jameskbride.codemashcompanion.R
 import com.jameskbride.codemashcompanion.framework.BaseActivity
 import com.jameskbride.codemashcompanion.framework.BaseActivityImpl
 
 class CodeOfConductActivityImpl: BaseActivityImpl() {
     override fun onCreate(savedInstanceState: Bundle?, qtn: BaseActivity) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        qtn.setContentView(R.layout.activity_code_of_conduct)
+
+        val webView = qtn.findViewById<WebView>(R.id.code_of_conduct_webview)
+        val webSettings = webView.settings
+        webSettings.builtInZoomControls = true
+        webView.webViewClient = CodeOfConductWebViewClient()
+        webView.loadUrl(qtn.resources.getString(R.string.code_of_conduct_url))
     }
 
     override fun onResume(sessionDetailActivity: BaseActivity) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onPause(sessionDetailActivity: BaseActivity) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class CodeOfConductWebViewClient: WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+        return false
     }
 }
---
+++ app/src/test/java/com/jameskbride/codemashcompanion/codeofconduct/CodeOfConductActivityImplTest.kt
@@ -0,0 +1,58 @@
+package com.jameskbride.codemashcompanion.codeofconduct
+
+import android.content.res.Resources
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.jameskbride.codemashcompanion.R
+import com.nhaarman.mockito_kotlin.argumentCaptor
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import org.junit.Assert.assertFalse
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations.initMocks
+
+class CodeOfConductActivityImplTest {
+
+    @Mock private lateinit var qtn:CodeOfConductActivity
+    @Mock private lateinit var codeOfConductWebView:WebView
+    @Mock private lateinit var webSettings:WebSettings
+    @Mock private lateinit var resources:Resources
+
+    private lateinit var subject:CodeOfConductActivityImpl
+
+    @Before
+    fun setUp() {
+        initMocks(this)
+
+        subject = CodeOfConductActivityImpl()
+
+        whenever(qtn.findViewById<WebView>(R.id.code_of_conduct_webview)).thenReturn(codeOfConductWebView)
+        whenever(codeOfConductWebView.settings).thenReturn(webSettings)
+        whenever(qtn.resources).thenReturn(resources)
+    }
+
+    @Test
+    fun onCreateInflatesTheView() {
+        subject.onCreate(null, qtn)
+
+        verify(qtn).setContentView(R.layout.activity_code_of_conduct)
+    }
+
+    @Test
+    fun onCreateSetsTheCodeOfConductUrl() {
+        val url = "http://www.codemash.org/codemash-code-conduct"
+        whenever(resources.getString(R.string.code_of_conduct_url)).thenReturn(url)
+
+        subject.onCreate(null, qtn)
+
+        verify(codeOfConductWebView).loadUrl(url)
+
+        val argumentCaptor = argumentCaptor<WebViewClient>()
+        verify(codeOfConductWebView).setWebViewClient(argumentCaptor.capture())
+
+        assertFalse(argumentCaptor.firstValue.shouldOverrideUrlLoading(null, ""))
+    }
+}
